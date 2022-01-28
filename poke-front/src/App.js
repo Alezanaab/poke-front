3b1ad8b8f414fb2e477bcb0e8317d70d022997bb
Alexia Abrego <aabrego@unitedvirtualities.com>
2022-01-27 23:07:31 -0700

    First structure poke front

---
+++ poke-front/src/App.js
@@ -0,0 +1,10 @@
+//import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1  class="text-3xl font-bold underline">Bookkeeper!</h1>
+    </div>
+  );
+ }
